a9d7e540970c317152e36f4312cefb110a96b0b3
vishnups0305 <vishnu@ineuron.ai>
2023-08-11 18:05:26 +0530

    updates

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import { connect } from './src/db/db';
 
 dotenv.config();
-const app = express.Router();
+const app = express();
 
+const myport = process.env.PORT
+app.listen(myport, async () => {
+    try {
+        const mongoURL = process.env.URL
+        if (mongoURL) await connect(mongoURL)
 
-app.use('/',)
+    } catch (error) {
+        console.log('crashed', error);
+    }
+    console.log('listening to the port ', myport)
+})
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,7 @@
 
 import mongoose from "mongoose";
 
-const mongoURL = process.env.URL
-export const connect = async () => {
-    if (mongoURL) return await mongoose.connect(mongoURL);
+
+export const connect = async (url: string) => {
+    return await mongoose.connect(url);
 }
