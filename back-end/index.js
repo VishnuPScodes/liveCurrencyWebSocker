fd5fecb43529943eff02a6d5e76b4621b891eae2
vishnups0305 <95961812+VishnuPScodes@users.noreply.github.com>
2023-09-18 15:19:12 +0530

    dd
    s

--- back-end/index.js
+++ back-end/index.js
@@ -1,9 +1,23 @@
 import express from "express";
 import { mongooseConnect } from "./src/db/db.js";
 import dotenv from 'dotenv';
+import { Server } from "socket.io";
+import http from 'http';
+
+const server = http.createServer();
+const io = new Server(server);
 dotenv.config();
 const app = express();
-
+io.on('connection', (socket) => {
+    console.log('one user connected');
+    socket.on('chatMessage', (msg) => {
+        console.log('recieved message', msg);
+        io.emit(msg);
+    });
+    socket.on('disconnect', (msg) => {
+        console.log('user disconnected');
+    })
+})
 const port = process.env.PORT;
 const url = process.env.URL;
 app.listen(port, async () => {
--- back-end/src/db/db.js
+++ back-end/src/db/db.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-
 export const mongooseConnect = async (url) => {
     console.log('url', url)
     return await mongoose.connect(url)
